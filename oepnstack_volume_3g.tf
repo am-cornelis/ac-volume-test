b152cc9c76a7fbdd0379b1335a2833a720b84f92
Jose A Dianes <jadianes@gmail.com>
2016-01-19 13:46:58 +0000

    Initial commit

---
+++ oepnstack_volume_3g.tf
@@ -0,0 +1,16 @@
+# Configure the OpenStack Provider
+provider "openstack" {
+#  The openstack configuration can be provided inside of the .tf file or using env variables.
+#  WARNING, early versions of terraform, store the unencrypted credentials in terraform state file.
+#    user_name = "OS_USERNAME"
+#    tenant_name = "OS_TENANT_NAME"
+#    auth_url = "OS_AUTH_URL"
+#    password = "OS_PASSWORD"
+}
+
+
+resource "openstack_blockstorage_volume_v1" "volume_1" {
+  name = "be-test-volume"
+  description = "first test volume"
+  size = 3
+}
